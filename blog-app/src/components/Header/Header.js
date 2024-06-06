1c8e10e2e222c418d89f29d8c377f0f9fcd52c85
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-06-06 21:20:02 +0500

    upd

---
+++ blog-app/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import "./header-style.css";
+
+export default function Header() {
+    return (
+        <div>
+            <div className="">
+
+            </div>
+        </div>
+    )
+}
