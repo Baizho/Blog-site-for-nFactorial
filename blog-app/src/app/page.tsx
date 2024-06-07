1230af35a2747db52660a230d990bf2dfe221430
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-06-07 10:50:20 +0500

    upd

--- blog-app/src/app/page.tsx
+++ blog-app/src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <div className="text-5xl bg-red-400 px-16">This is a div</div>
+    </>
+  );
 }
